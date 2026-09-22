0e1a9fb0396d63aac5c289cf3eb58197f61c073e
mirdev999 <davpmir@gmail.com>
2026-09-22 11:46:02 -0500

    Remove unused imports from main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, status, Response
+from fastapi import FastAPI, HTTPException
 from app.schemas import TransactionCreate, TransactionOut
 
 app = FastAPI()
