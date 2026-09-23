82ce5e7cc91930a427c52337703f90e3d855340d
mirdev999 <davpmir@gmail.com>
2026-09-23 11:14:16 -0500

    naming consistency

--- app/main.py
+++ app/main.py
@@ -16,11 +16,11 @@ async def create_transaction(transaction : TransactionCreate):
     return request_dict
 
 @app.get("/transactions",response_model=list[TransactionOut])
-async def list_transaction():
+async def list_transactions():
     return transactions_db
 
 @app.get("/transactions/{transaction_id}",response_model=TransactionOut)
-async def get_by_id(transaction_id:int):
+async def get_transaction(transaction_id:int):
     for transaction in transactions_db:
         if transaction["id"] == transaction_id:
             return transaction
